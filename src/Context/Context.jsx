5eefbc1aacd88183dbb2ec7b4fc8093b3922e89e
Farhan Aziz <mraziz459@gmail.com>
2024-11-02 01:09:45 +0500

    google done

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -2,15 +2,27 @@ import React, { createContext, useContext } from 'react'
 import { useState, useEffect } from 'react';
 import { auth } from '../firebase/firebase';
 import { onAuthStateChanged } from 'firebase/auth';
+import Loading from '../components/Loading/Loading';
 
 const BlogContext = createContext();
 const Context = ({ children}) => {
     const [currentUser, setCurrentUser] = useState(false)
+    const [loading, setLoading] = useState(true)
 
-  
+  useEffect(() => {
+      const unsubscribe = onAuthStateChanged(auth, (user) => {
+          if(user) {
+            setCurrentUser(user);
+          } else {
+            setCurrentUser(null);
+          }
+          setLoading(false)
+      })
+      return () => unsubscribe();
+  },[currentUser]);  
 
     return <BlogContext.Provider value={{ currentUser , setCurrentUser}}>
-    {children}
+    {loading ? <Loading /> : children}
     </BlogContext.Provider>
 }
 
